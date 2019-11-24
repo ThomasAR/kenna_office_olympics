844b21ea1015b3eca9c5b068dfd3dd0bfedc5c68
Thomas Arsenault <thomasarsenaultj@gmail.com>
2019-11-23 22:57:23 -0500

    Angle for shooting

--- js/game.js
+++ js/game.js
@@ -1,7 +1,14 @@
 var ball;
-var clicked = false;
 var rotateX = 0;
 var rotateY = 0;
+var landedX = 0;
+var landedY = 0;
+var angleThrow = 0;
+let angleSpeed = 2;
+
+let spaceDown = false;
+let spaceUp = false;
+
 function setup() {
 
     angleMode(DEGREES);
@@ -12,22 +19,67 @@ function setup() {
 }
 
 function draw() {
-    background(0);
+	background(0);
+		
+	if(spaceDown || spaceUp){
+		showPowerBar();
+	}
 
-    if (clicked) {
-        ball.show();
-    }
+    if (spaceUp) {
+		ball.show();
+	} else {
+		throwAngle();
+	}
+
+
 
 }
 
-function mouseClicked() {
+function throwAngle() {
+	if(angleThrow === 90) angleSpeed = -2;
+	if(angleThrow === 0) angleSpeed = 2;
+	angleThrow += angleSpeed;
+	translate(80, 1000)
+	rotate(angleThrow);
+	rect(0, 0, 20, 100);
+}
 
-    ball = new Ball();
-    clicked = true;
+function showPowerBar() {
+	if(spaceUp === false){
+
+		let powerBar = new PowerBar();
+	// ellipse(0, 0, 20, 100);
+	}
+}
+
+function keyPressed() {
+	if(key === " "){
+		spaceDown = true;
+	}
+}
+
+function keyReleased() {
+	if(key === " " && !spaceUp){
+		spaceUp = true;
+		spaceDown = false;
+		landedX = 0;
+		landedY = 0;
+		ball = new Ball();
+		spaceUp = true;
+	}
+
+}
+function PowerBar() {
+	let barStart = width / 2 - 300;
+	let barEnd = width / 2 + 300;
+	this.x = 400;
+	this.y = 400;
+	rect(barStart, 80, barEnd - barStart, 20);
 }
 
 function Ball() {
-    let angle = 25;
+	let angle = angleThrow;
+	console.log(angleThrow);
     let power = 100;
     this.x = 80;
     this.y = 1000;
@@ -39,26 +91,30 @@ function Ball() {
     this.gravity = 1;
 
     this.show = function () {
-        ellipse(this.x, this.y, this.r * 2, this.r * 2);
+
+		translate(this.x, this.y);
+		rotate(-1 * atan((landedX ? landedX : this.xSpeed) / (landedY ? landedY : this.ySpeed)));
+
+        rect(0, 0, 20, 100);
 
 
         this.ySpeed += this.gravity;
         this.y += this.ySpeed;
-        this.x += this.xSpeed;
-
-        rotate(80, 80, -1 * atan(this.xSpeed / this.ySpeed))
-        rect(80, 80, 10, 100);
+		this.x += this.xSpeed;
 
         if (this.y >= height - 10) {
-            this.y = height - 10;
-            this.xSpeed = 0;
+			this.y = height - 10;
+			landedX = landedX ? landedX : this.xSpeed;
+			landedY = landedY ? landedY : this.ySpeed;	
+			this.xSpeed = 0;
+			console.log(landedX);
+			noLoop();
         }
         else {
             rotateX = this.xSpeed;
             rotateY = this.ySpeed;
-            console.log(this.xSpeed, this.ySpeed);
 
-        }
+		}
     }
 }
 
