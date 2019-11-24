dd907c2a1e2874249a163535fa64a7e071ed0881
Thomas Arsenault <tarsenault@kenna.ca>
2019-11-23 20:32:34 -0500

    Added throwing physics

--- js/game.js
+++ js/game.js
@@ -1,4 +1,75 @@
+var ball;
+var clicked = false;
+var rotateX = 0;
+var rotateY = 0;
 function setup() {
+
+    angleMode(DEGREES);
+    rectMode(CENTER);
+
+    // console.log(query.substring())
+    createCanvas(1920, 1080)
+}
+
+function draw() {
+    background(0);
+
+    if (clicked) {
+        ball.show();
+    }
+
+}
+
+function mouseClicked() {
+
+    ball = new Ball();
+    clicked = true;
+}
+
+function Ball() {
+    let angle = 25;
+    let power = 100;
+    this.x = 80;
+    this.y = 1000;
+    this.r = 30;
+
+    this.xSpeed = power * sin(angle) * 0.4;
+    this.ySpeed = power * cos(angle) * -1 * 0.4;
+
+    this.gravity = 1;
+
+    this.show = function () {
+        ellipse(this.x, this.y, this.r * 2, this.r * 2);
+
+
+        this.ySpeed += this.gravity;
+        this.y += this.ySpeed;
+        this.x += this.xSpeed;
+
+        rotate(80, 80, -1 * atan(this.xSpeed / this.ySpeed))
+        rect(80, 80, 10, 100);
+
+        if (this.y >= height - 10) {
+            this.y = height - 10;
+            this.xSpeed = 0;
+        }
+        else {
+            rotateX = this.xSpeed;
+            rotateY = this.ySpeed;
+            console.log(this.xSpeed, this.ySpeed);
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+function getQueryStringParams() {
     let query = window.location.search;
 
     let nicknameSearch = "&nickname=";
@@ -16,15 +87,4 @@ function setup() {
     console.log(nickname);
     console.log(character);
     console.log(avatar);
-
-
-    // console.log(query.substring())
-}
-
-function draw() {
-    ellipse(50, 50, 80, 80);
-}
-
-function getQueryStringParams() {
-
 }
